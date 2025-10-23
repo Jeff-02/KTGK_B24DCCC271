9d483ffd7a49e9754ba6f086c2ccd1ec980dfc74
B24DCCC2721_Nguyễn Đức Trường <truongnguyenduc062@gmail.com>
2025-10-23 18:44:53 +0700

    Update CreatePost.tsx

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,15 +1,14 @@
-
 import PostForm from '../components/PostForm';
 import { Post } from '../types/Post';
-import { useNavigate } from 'react-router-dom'; //
-import { v4 as uuidv4 } from 'uuid'; //
+import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 interface CreatePostProps {
   setPosts: React.Dispatch<React.SetStateAction<Post[]>>;
 }
 
 const CreatePost = ({ setPosts }: CreatePostProps) => {
-  const navigate = useNavigate(); //
+  const navigate = useNavigate();
 
   const handleCreate = (newPostData: Omit<Post, 'id' | 'createdAt'>) => {
     const newPost: Post = {
@@ -19,18 +18,13 @@ const CreatePost = ({ setPosts }: CreatePostProps) => {
     };
     
     setPosts((prevPosts) => [newPost, ...prevPosts]);
-    
     alert('Đăng bài thành công!');
-    navigate('/'); //
-  };
-
-  const handleSubmit = (data: Omit<Post, 'id' | 'createdAt'> | Post) => {
-    handleCreate(data as Omit<Post, 'id' | 'createdAt'>);
+    navigate('/');
   };
 
   return (
     <PostForm 
-      onSubmit={handleSubmit} 
+      onSubmit={handleCreate} 
       mode="create" 
     />
   );
